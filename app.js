b308c7d360c90835f11a50de4983c6c280c2d234
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-06 15:02:07 -0400

    now working to add book to collection and have it display on screen

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 const Books = require('./models/books');
 const indexController = require('./controllers/indexController');
 
@@ -9,20 +10,29 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 // all things mongoose
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/bookcollection');
 
 // requests
-
+// render index page and all books in db
 app.get('/', indexController.renderAllIndex);
+
+// add a new book to the collection and have it added to the list when the page is refreshed
+app.post('/add', indexController.addBooks);
+
+// go to detail page when click on book
+app.post('/detail/:id', indexController.toDetail);
 
 app.listen(3000, function() {
   console.log('Successfully started express application');
 });
 
-// -------ADDING DATA TO DATABASE
+// -------ADDING DATA TO DATABASE (had to redo from earlier project)
 // book not in a series
 // const book = new Books({
 //   title: 'The Truth About Forever'
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,6 @@
 const Books = require('../models/books');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 module.exports = {
   renderAllIndex: function(req, res) {
@@ -6,5 +8,17 @@ module.exports = {
       console.log(results);
       res.render('index', {model: results});
     });
+  }
+  , addBooks: function(req, res) {
+    const book = new Books({
+      title: req.body.bookTitle
+      , author: req.body.author
+    });
+    book.save();
+    res.redirect('/');
+  }
+  , toDetail: function(req, res) {
+    var id = req.params.id;
+    Books.findOne({_id: id})
   }
 };
